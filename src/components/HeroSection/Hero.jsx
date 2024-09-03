9f6d1d681f65f5b125204706ab968ef979c67f53
rudresh-dev <rudresh@craftech360.com>
2024-09-03 18:40:14 +0530

    added

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -2,21 +2,21 @@
 const Hero = () => {
   return (
     <div className="bg-[#CBEEFF] w-screen flex justify-center flex-col md:flex-row md:justify-center lg:justify-center bg-[url('/bghero.png')] xl:gap-5 lg:gap-16 bg-center pt-8 pb-10 items-center px-8 gap-10">
-        <div className="w-[400px] px-8 lg:px-5">
+        <div className=" px-8 lg:px-5">
             <p className="text-xl mb-2">Key Areas:</p>
-            <ul className="text-[17]">
-                <li>1. Breakout Theatre 1 - Grand Ballroom 1</li>
-                <li>2. Breakout Theatre 2 - Grand Ballroom 3</li>
-                <li>3. Breakout Theatre 3 - Royal Ballroom 1</li>
-                <li>4. Breakout Theatre 4 - Royal Ballroom 2 </li>
-                <li>5. Breakout Theatre 5 - Grand Ballroom 2</li>
-                <li>6. Al-First Expo - Pre-function Area</li>
-                <li>7. Expo Theatres - Studio 3 & Royal Ballroom 1-2</li>
-                <li>8. Breakout Theatre 1 - Grand Ballroom 1</li>
+            <ul className="text-[14]">
+                <li className="text-[14]">1. Breakout Theatre 1 - Grand Ballroom 1</li>
+                <li className="text-[14]">2. Breakout Theatre 2 - Grand Ballroom 3</li>
+                <li className="text-[14]">3. Breakout Theatre 3 - Royal Ballroom 1</li>
+                <li className="text-[14]">4. Breakout Theatre 4 - Royal Ballroom 2 </li>
+                <li className="text-[14]">5. Breakout Theatre 5 - Grand Ballroom 2</li>
+                <li className="text-[14]">6. Al-First Expo - Pre-function Area</li>
+                <li className="text-[14]">7. Expo Theatres - Studio 3 & Royal Ballroom 1-2</li>
+                <li className="text-[14]">8. Breakout Theatre 1 - Grand Ballroom 1</li>
             </ul>
         </div>
-        <div className="md:block lg:block h-full">
-            <img src="/img112.png" alt="" className="h-full" />
+        <div className="md:block lg:block h-full w-full">
+            <img src="/img112.png" alt="" className="h-full w-full" />
         </div>
     </div>
   )
