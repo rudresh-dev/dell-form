716bc835f6c3dcf848981a0d53a08dffb2aa7643
rudresh-dev <rudresh@craftech360.com>
2024-09-04 11:47:16 +0530

    kkll

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -2,7 +2,7 @@
 const Hero = () => {
   return (
     <div className="bg-[#CBEEFF] w-screen flex justify-center flex-col md:flex-row md:justify-center lg:justify-center bg-[url('/bghero.png')] xl:gap-5 lg:gap-16 bg-center pt-8 pb-10 items-center px-8 gap-10">
-        <div className=" px-6 lg:px-5">
+        <div className=" px-0 lg:px-5">
             <p className="text-xl mb-2">Key Areas:</p>
             <ul className="text-[14]">
                 <li className="text-[14]">1. Breakout Theatre 1 - Grand Ballroom 1</li>
